e96004affe3fb3891cfe75cf782fdb45ccce0357
LAbeleda-9T <abeleda.lg@obmontessori.edu.ph>
2026-02-21 14:40:50 +0800

    Add files via upload

---
+++ main skt.py
@@ -0,0 +1,24 @@
+# Account Creation
+from pyscript import display, document
+
+
+def account_authentication(e):
+    document.getElementById('output').innerHTML = ""
+
+    username = document.getElementById('input1').value
+    password = document.getElementById('input2').value
+
+    if len(username) < 7 and len(password) < 10:
+        display("Username must be at least 7 characters and password must be at least 10 characters.", target="output")
+
+    elif len(username) < 8:
+        display("Username must be at least 8 characters.", target="output")
+
+    elif password.isalpha():
+        display("Password must contain at least one number.", target="output")
+
+    elif password.isdigit():
+        display("Password must contain at least one letter.", target="output")
+
+    else:
+        display("Your username and password are valid.", target="output")
